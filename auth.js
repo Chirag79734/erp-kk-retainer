fe8e7e8bf337c74cecf1537138c80ca565b4712b
Chirag <chirag@example.com>
2026-07-12 10:11:41 -0700

    feat: Add Firebase Google Authentication

---
+++ auth.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEOJxOOnyk6CbKppp2Z1TOGnxrH4U7yVs",
+  authDomain: "retainer-stats-pfx.firebaseapp.com",
+  projectId: "retainer-stats-pfx",
+  storageBucket: "retainer-stats-pfx.firebasestorage.app",
+  messagingSenderId: "496931811879",
+  appId: "1:496931811879:web:a736f75aa9244cd8dc9402"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, signInWithPopup, signOut, onAuthStateChanged };
